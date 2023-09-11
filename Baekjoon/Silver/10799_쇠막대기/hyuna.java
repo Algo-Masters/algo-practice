b7d5b770f6941d045ed07f64bc9d1da2b8ba97f0
hyuna <monicajo0@naver.com>
2023-09-11 18:54:50 +0900

    Create 쇠막대기 hyuna.java

---
+++ Baekjoon/Silver/10799_쇠막대기/hyuna.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 백준 q10799 - 쇠막대기
+public class Main {
+    public static String input;
+    public static int answer = 0;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        input = br.readLine();
+        cutBar();
+        bw.write(Integer.toString(answer));
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+
+    public static void cutBar(){
+        int currentCnt = 0;
+        char priorChar = '(';
+
+        int inputLen = input.length();
+        for(int i=0;i<inputLen;i++){
+            char ch = input.charAt(i);
+
+            if(ch == '('){
+                currentCnt++;
+                priorChar = '(';
+            }
+            else if(priorChar == ')'){ // 쇠막대기 오른쪽 끝점
+                currentCnt--;
+                answer += 1;
+            }
+            else{ // 레이저인 경우 priorChar=='('
+                currentCnt--;
+                answer += currentCnt;
+                priorChar = ')';
+            }
+        }
+    }
+}
